18f1b6d4bb74a01dd1b2a93d9e26dc152f561101
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-05-28 15:44:36 -0400

    Node server, GitHub authentication working

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,26 @@
   'use strict';
 
   $(document).ready(function(){
-    $('body').prepend(JST['application']());
+    $.ajax({
+      url: "https://api.github.com/user",
+      headers: {
+        "Authorization": "token " + GITHUB_TOKEN
+      }
+    }).then(function(user) {
+      console.log(user);
+    });
+  });
+
+  $(document).ready(function(e){
+    var code = window.location.href.match(/\?code=(.*)/)[1];
+    if(code) {
+      $.getJSON('http://localhost:9999/authenticate/'+code, function(data) {
+       console.log(data.token);
+      });
+    }
+  });
+
+  $('button').on('click', function(e){
+    window.location.replace('https://github.com/login/oauth/authorize?client_id=160a2a024037bccec612');
   });
 })();
---
+++ scripts/token.js
@@ -0,0 +1,5 @@
+(function(){
+ 'use strict';
+
+ window.GITHUB_TOKEN = "f963759f6e7558053bf6603adda88a13af142f07";
+})();
