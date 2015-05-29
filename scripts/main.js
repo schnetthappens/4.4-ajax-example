fb18518c969abec0142d3ddc08147d13f3807176
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-05-29 15:03:46 -0400

    Add octicons link to head

--- scripts/main.js
+++ scripts/main.js
@@ -16,12 +16,12 @@
     var code = window.location.href.match(/\?code=(.*)/)[1];
     if(code) {
       $.getJSON('http://localhost:9999/authenticate/'+code, function(data) {
-       console.log(data.token);
+       console.log(data);
       });
     }
   });
 
-  $('button').on('click', function(e){
-    window.location.replace('https://github.com/login/oauth/authorize?client_id=160a2a024037bccec612');
-  });
+  // $('button').on('click', function(e){
+  //   window.location.replace('https://github.com/login/oauth/authorize?client_id=160a2a024037bccec612');
+  // });
 })();
