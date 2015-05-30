3fb7054ff8b18b70994accdca501a837f8b3f2a3
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-05-30 13:39:38 -0400

    Make AJAX calls, console.log user and repo data

--- scripts/main.js
+++ scripts/main.js
@@ -2,26 +2,18 @@
   'use strict';
 
   $(document).ready(function(){
-    $.ajax({
-      url: "https://api.github.com/user",
-      headers: {
-        "Authorization": "token " + GITHUB_TOKEN
-      }
-    }).then(function(user) {
-      console.log(user);
-    });
+
+  $.ajax({
+    url: "https://api.github.com/users/schnetthappens",
+    }).then(function(data){
+    console.log(data);
   });
 
-  $(document).ready(function(e){
-    var code = window.location.href.match(/\?code=(.*)/)[1];
-    if(code) {
-      $.getJSON('http://localhost:9999/authenticate/'+code, function(data) {
-       console.log(data);
-      });
-    }
+  $.ajax({
+    url: "https://api.github.com/users/schnetthappens/repos",
+    }).then(function(data){
+    console.log(data);
   });
+});
 
-  // $('button').on('click', function(e){
-  //   window.location.replace('https://github.com/login/oauth/authorize?client_id=160a2a024037bccec612');
-  // });
 })();
